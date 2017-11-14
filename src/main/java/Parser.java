795b1adc11f98e40c9c78e07837f0a86ee6d9b73
Vladislav Kozachenko <vladislav.kozachenko@teamdev.local>
2017-11-14 18:47:42 +0200

    Added tests for Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * Parses input and returns array of commands.
  */
 public class Parser {
+
+    public ArrayList<Command> parse(String sourceCode){
+        return null;
+    }
 }
--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,2 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ParserTest {
+
+    @Test
+    public void parseTest(){
+        Parser parser = new Parser();
+        ArrayList<Command> commands = parser.parse("-+<>[].");
+        Assert.assertEquals(Command.DECREMENT, commands.get(0));
+        Assert.assertEquals(Command.INCREMENT, commands.get(1));
+        Assert.assertEquals(Command.MOVE_LEFT, commands.get(2));
+        Assert.assertEquals(Command.MOVE_RIGHT, commands.get(3));
+        Assert.assertEquals(Command.LOOP_START, commands.get(4));
+        Assert.assertEquals(Command.LOOP_END, commands.get(5));
+        Assert.assertEquals(Command.OUTPUT, commands.get(6));
+    }
 }
