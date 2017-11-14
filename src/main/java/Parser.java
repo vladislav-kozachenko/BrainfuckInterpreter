852681397b0966178cd120d0178eb3253d4a7ddb
Vladislav Kozachenko <vladislav.kozachenko@teamdev.local>
2017-11-14 19:09:41 +0200

    Implemented Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import com.google.common.base.Strings;
 
 /**
  * Parses input and returns array of commands.
@@ -6,6 +7,35 @@ import java.util.ArrayList;
 public class Parser {
 
     public ArrayList<Command> parse(String sourceCode){
-        return null;
+        ArrayList<Command> commands = new ArrayList<>();
+        if (Strings.isNullOrEmpty(sourceCode)){
+            throw new RuntimeException();
+        }
+        for (char command : sourceCode.toCharArray()) {
+            switch (command){
+                case '+':
+                    commands.add(Command.INCREMENT);
+                    break;
+                case '-':
+                    commands.add(Command.DECREMENT);
+                    break;
+                case '<':
+                    commands.add(Command.MOVE_LEFT);
+                    break;
+                case '>':
+                    commands.add(Command.MOVE_RIGHT);
+                    break;
+                case '[':
+                    commands.add(Command.LOOP_START);
+                    break;
+                case ']':
+                    commands.add(Command.LOOP_END);
+                    break;
+                case '.':
+                    commands.add(Command.OUTPUT);
+                    break;
+            }
+        }
+        return commands;
     }
 }
